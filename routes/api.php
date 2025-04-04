4e54cd59275fc2e04ac80ab8f0c0131290e7ba1d
Igor C <igor.chausse@etu.hesge.ch>
2025-04-04 11:51:50 +0200

    Feat: ajout de l'authentification API avec Laravel Sanctum et création du contrôleur AuthController pour la gestion des connexions

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+use App\Http\Controllers\AuthController;
+
+Route::get(uri: '/user', action: function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post(uri: '/login', action: [AuthController::class, 'login']);
